9739a6cd61c0ed20aef464e624a0d7776d270698
Marvin Buss <marvin.buss@gmail.com>
2020-03-06 11:25:31 +0100

    updated inputs and file names

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import json
 
 
 def main():
+    # Loading input values
+    print("::debug::Loading input values")
+    parameters_file = os.environ.get("INPUT_PARAMETERSFILE", default=None)
+
     # Loading parameters file
     print("::debug::Loading parameters file")
     parameters_file_path = os.path.join(".aml", parameters_file)
@@ -12,7 +16,9 @@ def main():
     except FileNotFoundError:
         print(f"::error::Could not find parameter file in {parameters_file_path}. Please provide a parameter file in your repository (e.g. .aml/workspace.json).")
         return
-    
+
+    # TODO: Create compute if not existing.
+    print(parameters)
     print("::debug::Successfully finised Azure Machine Learning Compute Action")
 
 
